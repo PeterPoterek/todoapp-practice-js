c78a7a5d820641c4ba7b1cc6135684f3055599a4
Piotr Poterek <peterpoterek0@gmail.com>
2023-10-27 20:35:10 +0200

    renderTaskList function added

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const taskForm = document.querySelector("#taskForm");
 const newTaskInput = document.querySelector("#newTaskInput");
 const newTaskButton = document.querySelector("#newTaskButton");
+const taskListUI = document.querySelector("#taskList");
+const taskList = [];
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  renderTaskList();
+});
 
 newTaskInput.addEventListener("change", (e) => {
+  e.preventDefault();
   let newTask = e.target.value;
-  console.log(newTask.length);
   if (newTask === " " || newTask.length === 0) {
     newTaskButton.setAttribute("disabled", true);
   } else {
     newTaskButton.removeAttribute("disabled");
   }
+
+  taskList.push({ taskDesc: newTask, completed: false });
+  console.log(taskList);
 });
+
+const renderTaskList = () => {
+  taskList.map((task) => {
+    console.log(task);
+    const taskUI = document.createElement("li");
+    const completeTaskButton = document.createElement("button");
+
+    taskUI.innerHTML = task.taskDesc;
+    completeTaskButton.innerHTML = "Completed";
+    taskUI.append(completeTaskButton);
+
+    taskListUI.append(taskUI);
+  });
+};
